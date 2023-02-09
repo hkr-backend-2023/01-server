3c1a24c8c547e312e50d120014f4772ae8f0ef9d
David Andersson <david.andersson@zocom.se>
2023-02-09 10:54:18 +0100

    Basic server added

---
+++ server.js
@@ -0,0 +1,20 @@
+// If you want to use the more modern "import" syntax, remember to add "type": "module" to package.json
+const express = require('express')
+
+const app = express()
+
+
+let visitCount = 0
+
+app.get('/', (req, res) => {
+	console.log('GET request for / ')
+	visitCount++
+	// Three methods of using strings in JavaScript: '', "", ``
+	res.send(`Hello! This is the ${visitCount} time you have visited.`)
+})
+// 
+
+
+app.listen(3000, () => {
+	console.log('Server is listening on port 3000...')
+})
