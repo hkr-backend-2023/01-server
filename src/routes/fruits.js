5fea46fe29fdba8541e11704ceb5ea831ca6ba34
David Andersson <david.andersson@zocom.se>
2023-02-09 15:24:38 +0100

    Delete route complete

--- src/routes/fruits.js
+++ src/routes/fruits.js
@@ -1,6 +1,6 @@
 
 // In a more realistic example, this data would be in a database
-const fruitData = [
+let fruitData = [
 	{ name: 'apple', price: 20, id: 1 },
 	{ name: 'banana', price: 35, id: 2 }
 ]
@@ -17,27 +17,49 @@ const specificProductRoute = (req, res) => {
 	const index = Number(req.params.index)
 	console.log('GET request to /fruits/:index, index=' + index)
 	
+	if( validateIndex(index, res) ) {
+		res.send( fruitData[index] )
+	}
+	// else - validateIndex has already sent a response to the client
+}
+function validateIndex(index, res) {
 	// index can be: a valid number (index within the array), an invalid number (outside the array), not a number (NaN)
-	if( isNaN(index) ) {
+	if (isNaN(index)) {
 		console.log('- index is not a number at all')
 		// Tell the client that this was a BAD REQUEST
 		res.sendStatus(400)  // bad request
-		return
+		return false
 	}
-	else if( index < 0 || index >= fruitData.length ) {
+	else if (index < 0 || index >= fruitData.length) {
 		// Task: check if index is within or outside the array
 		// Array is numbered from 0 to length-1
 		console.log('- index is outside the array')
 		res.sendStatus(404)  // no fruit found
-		return
+		return false
 	}
 	else {
 		// All is ok
 		const fruit = fruitData[index]
 		console.log('- we found this fruit: ', fruit)
-		res.send(fruit)
-		return
+		// res.send(fruit)
+		return true
+		// If true, leave it up to the calling function to send a response
 	}
+
+}
+
+// URL in app.get must be: /fruits/:index
+const deleteProductRoute = (req, res) => {
+	const index = Number(req.params.index)
+	console.log('DELETE request to /fruits/:index, index=' + index)
+
+	if (validateIndex(index, res)) {
+		// Remove the fruit using the higher order function: filter
+		// Alternative method: using splice
+		fruitData = fruitData.filter((fruit, i) => i !== index)
+		res.sendStatus(200)
+	}
+	// else - validateIndex has already sent a response to the client
 }
 
 /*
@@ -47,4 +69,4 @@ let string = String(123)
 let mystery = '10' + 1  // automatic type conversion
 */
 
-module.exports = { allProductsRoute, specificProductRoute }
+module.exports = { allProductsRoute, specificProductRoute, deleteProductRoute }
--- src/server.js
+++ src/server.js
@@ -1,7 +1,7 @@
 // If you want to use the more modern "import" syntax, remember to add "type": "module" to package.json
 const express = require('express')
 const { indexRoute, helloRoute } = require('./routes/basic.js')
-const { allProductsRoute, specificProductRoute } = require('./routes/fruits.js')
+const { allProductsRoute, specificProductRoute, deleteProductRoute } = require('./routes/fruits.js')
 // Deconstructing an object: const { x, y } = object
 
 const app = express()
@@ -16,6 +16,8 @@ app.get('/hello', helloRoute)
 app.get('/fruits', allProductsRoute)
 
 app.get('/fruits/:index', specificProductRoute)
+
+app.delete('/fruits/:index', deleteProductRoute)
 
 
 
